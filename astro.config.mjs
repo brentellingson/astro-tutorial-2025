02aba693b7abd5732327471d8935586187d0eb29
Brent Ellingson <brent.ellingson@gmail.com>
2026-01-18 15:11:08 -0600

    chore: adding the github pages base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://brentellingson.github.io',
+    base: '/astro-tutorial-2025',
 });
