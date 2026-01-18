4ec1491175fd6b569c866178273a4f7545960bf2
Brent Ellingson <brent.ellingson@gmail.com>
2026-01-18 15:02:36 -0600

    fix: try pages with different url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://brentellingson.github.io/astro-tutorial-2025/',
+    site: 'https://brentellingson.github.io',
 });
