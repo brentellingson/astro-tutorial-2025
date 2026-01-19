91cce63a5d58c40aa172b47469059b023f24c737
Brent Ellingson <brent.ellingson@gmail.com>
2026-01-18 21:29:44 -0600

    chore: add blog page and style sheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://brentellingson.github.io',
-    base: '/astro-tutorial-2025',
+    base: '/astro-tutorial-2025/',
 });
